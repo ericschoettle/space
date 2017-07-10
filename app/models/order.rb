e0d126b061237156808559d1e12d5490235b1863
Eric Schoettle and Max Reichman <eric+max@>
2017-07-10 14:30:44 -0700

    Users can add and remove items from shopping cart. Total price and status are displayed for orders.

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,23 @@ class Order < ApplicationRecord
   has_many :products, through: :order_products
   belongs_to :account
   # validates :status, :total_price, :account_id, :presence => true
+
+  before_save :update_total
+  before_save :update_status
+
+  def calculate_total
+    self.order_products.collect { |order_product| order_product.product.price * order_product.quantity }.sum
+  end
+
+  private
+
+  def update_status
+    if self.status == nil
+      self.status = "In progress"
+    end
+  end
+
+  def update_total
+    self.total_price = calculate_total
+  end
 end
